b9f5172370343629e276e4b4774412ffb0a45fe9
pandichef <rashtyda@gmail.com>
2023-12-06 10:47:14 -0500

    STATIC_ROOT & MEDIA_ROOT

--- openai_django/openai_django/settings.py
+++ openai_django/openai_django/settings.py
@@ -113,6 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "/home/chatpr/static"  # prod only
+MEDIA_URL = "media/"
+MEDIA_ROOT = "/home/chatpr/media"  # prod only
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
