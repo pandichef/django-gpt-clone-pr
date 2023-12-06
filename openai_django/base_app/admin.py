dc32903b6d194b006f6edabc032cde3bcba5f5ca
pandichef <rashtyda@gmail.com>
2023-12-06 10:26:02 -0500

    my first commit

--- openai_django/base_app/admin.py
+++ openai_django/base_app/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, SourceType, Example  # Import your model here
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # add_form = CustomUserCreationForm
+    # form = CustomUserChangeForm
+    model = CustomUser
+    # list_display = [
+    #     "email",
+    #     "username",
+    # ]
+
+
+class SourceTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+class ExampleAdmin(admin.ModelAdmin):
+    list_display = (
+        "prompt_text",
+        "completion_text",
+        "is_approved",
+        "created_by",
+        "created_at",
+        "updated_at",
+        "reference",
+        "source_type",
+    )
+    list_filter = ("is_approved", "created_by")
+    search_fields = ("prompt_text", "completion_text")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(SourceType, SourceTypeAdmin)
+admin.site.register(Example, ExampleAdmin)
--- openai_django/base_app/models.py
+++ openai_django/base_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    pass
+
+
+class SourceType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Example(models.Model):
+    prompt_text = models.TextField()
+    completion_text = models.TextField()
+    is_approved = models.BooleanField(
+        default=False, help_text="Approved for fine-tuning"
+    )
+    created_by = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    source_type = models.ForeignKey(
+        SourceType,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        help_text="The source of the record",
+    )
+    reference = models.URLField(
+        null=True, blank=True, help_text="Not used in fine-tuning"
+    )
+    # reference = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"Example created by {self.created_by.username} on {self.created_at}"
