ae0a41e185dc0f583151a7e1d1081fe7d219caaf
pandichef <rashtyda@gmail.com>
2023-12-08 07:35:18 -0500

    add prompt engineering

--- openai_django/base_app/oai_queries.py
+++ openai_django/base_app/oai_queries.py
@@ -2,13 +2,22 @@
 from django.conf import settings
 import os
 import openai
-from .models import FineTuningJob
+from .models import FineTuningJob, Example
 
 # OpenAI API Key
 # if settings.OPENAI_API_KEY:
 # openai.api_key = settings.OPENAI_API_KEY
 # else:
 #     raise Exception("OpenAI API Key not found")
+
+
+def collate_prior_prompts():
+    # todo: limit examples by rank
+    eaxmples = Example.objects.all()
+    database_prompts = ""
+    for example in eaxmples:
+        database_prompts += f"Prompt:\n{example.prompt_text}\n\nCompletion:\n{example.completion_text}\n\n"
+    return database_prompts
 
 
 def get_completion(prompt):
@@ -18,11 +27,15 @@ def get_completion(prompt):
 
         client = OpenAI()
 
+        prompt_plus = collate_prior_prompts() + f"Prompt:\n{prompt}\n\nCompletion:\n"
+        # print(prompt_plus)
+        # prompt_plus = prompt
+
         completion = client.chat.completions.create(
             model=lastest_openai_model,
             messages=[
                 {"role": "system", "content": settings.SYSTEM_CONTENT,},
-                {"role": "user", "content": prompt},
+                {"role": "user", "content": prompt_plus},
             ],
         )
         # print(completion)
