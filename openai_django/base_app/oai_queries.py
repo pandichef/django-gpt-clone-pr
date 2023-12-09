754c97ff174d0d671a37025221ec6a5ec4d0fe2d
pandichef <rashtyda@gmail.com>
2023-12-08 20:25:22 -0500

    few shot setup

--- openai_django/base_app/oai_queries.py
+++ openai_django/base_app/oai_queries.py
@@ -31,7 +31,7 @@ def make_full_prompt(example):
 #     raise Exception("OpenAI API Key not found")
 
 
-def collate_prior_prompts(prompt, return_size=3):
+def collate_prior_prompts(prompt, return_size=5):
     # todo: limit examples by rank
     examples = Example.objects.all()
     examples_list = []
